9646472e998bced687dc06237151cde742c05dbe
Ramya Mahesh <ramya.mahesh@sjsu.edu>
2022-12-13 20:22:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,30 @@ import matplotlib.pyplot as plt
 
 st.title('Credit Card Fraud Detection')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://drive.google.com/file/d/1HiFyESnf22FeI2FBNR29z6Oq0C9zoQiC/view?usp=sharing')
+#DATE_COLUMN = 'date/time'
+#DATA_URL = ('https://drive.google.com/file/d/1HiFyESnf22FeI2FBNR29z6Oq0C9zoQiC/view?usp=sharing')
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(uploaded_file)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data 
